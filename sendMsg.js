5c7731f5e0998de2188f21d8d52d6840640d8a45
inna-n-a <abv.xyz@yandex.ru>
2023-12-12 01:58:51 +0300

    добавлена фуекция оптимизации сайта под разные разрешения экранов

---
+++ sendMsg.js
@@ -0,0 +1,8 @@
+document.getElementById('add-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // вызов фунции проверки
+    
+        alert("Сообщение отправлено!")
+   
+})
